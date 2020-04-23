a8efbff3b03d6a2935d9c6f91f7be6141319095d
Brayandotexe <brayanfrank.sm@gmail.com>
2020-04-23 17:39:07 -0300

    Adicionando Jumping on the Clouds

---
+++ src/JumpingOnTheClouds.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class JumpingOnTheClouds {
+
+    static int jumpingOnClouds(int[] c) {
+    	int jumps = 0;
+    	for(int i=0;i<c.length;i++) {
+    		if(i < c.length - 2 ) {
+    			if(c[i + 2] == 0)i++;
+    			jumps++;
+    		}
+    		else if(i < c.length - 1) jumps++;
+    	}
+    	return jumps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(scanner.nextLine());
+        
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        System.out.println(jumpingOnClouds(c));
+
+       
+    }
+}
